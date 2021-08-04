31386c863b562efdcf128b0fa0a62527b4819468
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-08-04 18:15:37 +0300

    Implement add todo and clear completed todos

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Click to edit a todo</p>
+      <p>
+        Created by <a href="https://github.com/zkurucu">Zafer Kurucu</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Todo/index.js
@@ -0,0 +1,61 @@
+import Footer from "./Footer";
+import Form from "./Form";
+import List from "./List";
+import React, { useState } from "react";
+
+const initialTodos = [
+  { text: "Learn Javascript", isCompleted: true },
+  { text: "Learn React", isCompleted: false },
+  { text: "Have a life!", isCompleted: false },
+];
+
+const checkFilter = (filter, item) => {
+  switch (filter) {
+    case "all":
+      return true;
+    case "active":
+      return !item.isCompleted;
+    case "completed":
+      return item.isCompleted;
+    default:
+      return false;
+  }
+};
+
+const getUncompletedTodoCount = (todos) => {
+  return todos.filter((todo) => !todo.isCompleted).length;
+};
+
+function Todo() {
+  const [todos, setTodos] = useState(initialTodos);
+  const [filter, setFilter] = useState("all");
+
+  const removeAllCompletedTodos = () => {
+    const filteredTodos = todos.filter((todo) => !todo.isCompleted);
+    setTodos(filteredTodos);
+  };
+  return (
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <Form todos={todos} setTodos={setTodos} />
+      </header>
+
+      <List
+        todos={todos}
+        setTodos={setTodos}
+        filter={filter}
+        checkFilter={checkFilter}
+      />
+
+      <Footer
+        filter={filter}
+        setFilter={setFilter}
+        uncompletedTodoCount={getUncompletedTodoCount(todos)}
+        clearCompletedTodos={removeAllCompletedTodos}
+      />
+    </section>
+  );
+}
+
+export default Todo;
