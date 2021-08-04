55df7acf2062e45b40f2253422deb213203bdf31
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-08-04 18:49:43 +0300

    Update footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Todo from "./components/Todo";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Todo />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
